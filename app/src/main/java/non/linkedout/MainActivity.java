f3c1189f4997ceea5eca65d9d4098bf7bc959ad2
ace <a>
2018-11-10 18:25:39 -0500

    :')

--- app/src/main/java/non/linkedout/MainActivity.java
+++ app/src/main/java/non/linkedout/MainActivity.java
@@ -1,13 +1,39 @@
 package non.linkedout;
 
+import android.content.Context;
+import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button addButton;
+    private EditText number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        number = findViewById(R.id.number);
+        addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String amount = number.toString();
+                String[] epic = amount.split(" ");
+                int sum = 0;
+                for (String num:epic){
+                    sum =  sum + Integer.parseInt(num);
+                }
+                Context context = getApplicationContext();
+                CharSequence text = Integer.toString(sum);
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
+
 }
